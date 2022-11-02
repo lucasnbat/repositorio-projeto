d3502bde51fd0846a892cfc14a84eed6f8f71524
Lucas <lucasnunesb6@gmail.com>
2022-11-02 13:22:34 -0400

    Adição de comentarios para explicações do código

--- vetorX.java
+++ vetorX.java
@@ -13,17 +13,17 @@ public void inserir (long  elemento){
 }
 public void pesquisar(long chave) {
 	int i;
-	for (i = 0; i <= nElementos - 1; i++) {
-		if (vetor[i] == chave) {
-			System.out.print("Retorno da pesquisa=" + vetor[i]);
+	for (i = 0; i <= nElementos - 1; i++) { //percorre todo o vetor
+		if (vetor[i] == chave) { //caso encontre o elemento desejado
+			System.out.print("Retorno da pesquisa=" + vetor[i]); //retorna essa mensagem e para
 			break;
 		}
 	}
-	if (i == nElementos) {
-		System.out.print("Retorno da pesquisa= não encontrado");	
+	if (i == nElementos) { //se percorreu tudo e não encontrou
+		System.out.print("Retorno da pesquisa= não encontrado");//retorna essa mensagem	
 	}
 }
-public void imprimir(){
+public void imprimir(){ //metodo basico que usa i para percorrer o vaetor e ir imprimindo os elementos todos em uma linha, espaçados
 	for (int i = 0; i<nElementos; i++){
         	System.out.print(vetor[i] + " ");
         }
@@ -31,21 +31,21 @@ public void imprimir(){
 public boolean apagar(long elemento) {
 	int j;
 	for (j = 0; j < nElementos-1; j++) {
-		if (elemento == vetor[j]) {
-			break;
+		if (elemento == vetor[j]) { //usa variavel j para percorrer o vetor até achar o elemento desejado
+			break; //caso encontrar, pare e vai para proxima linha
 		}
 	}
-	if (j == nElementos) {
-		return false;
-	} else {
-		for (int k = j; k < nElementos-1; k++) {
+	if (j == nElementos) { //se percorrer tudo e não encontrar,
+		return false; //retorna false
+	} else { //se encontrou...
+		for (int k = j; k < nElementos-1; k++) { //vai pegar o proximo elemento e puxar para o indice anterior de forma a sobrepor o elem.
 			vetor[k] = vetor[k + 1];
 		}
-		nElementos--;
-		return true;
+		nElementos--; //numero de elementos é reduzido
+		return true; //retorna true
 	}
 }
-public int getTamanho() {
+public int getTamanho() { //retorna o tamanho do vetor usando o .length do java
 	return vetor.length;
 }
 }
